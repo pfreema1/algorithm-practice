211219d73c4c1665fab6eedc20ca0c18f3d1c9ea
Paul <pfreema1@gmail.com>
2018-01-30 19:46:20 -0500

    revint done

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let revNum = n
+    .toString()
+    .split("")
+    .reduce((prevVal, elem) => elem + prevVal);
+
+  if (revNum.charAt(revNum.length - 1) === "-") {
+    revNum = "-" + revNum.slice(0, revNum.length - 1);
+    console.log(parseInt(revNum));
+    return parseInt(revNum);
+  } else {
+    console.log(revNum);
+    return parseInt(revNum);
+  }
+
+  console.log(revNum);
+}
+
+reverseInt(123);
+reverseInt(-15);
 
 module.exports = reverseInt;
